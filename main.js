eed85d74d8d4b91c425d5b55c8910cd8b83c4637
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-09-27 20:51:03 +0530

    loco scroll

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-const videoConAnime = ()=>{
-    const videoContainer = document.querySelector(".video-container");
-const play = document.querySelector(".play");
+const loco = () => {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+};
+loco();
 
-videoContainer.addEventListener("mouseenter", () => {
-  gsap.to(play, {
-    opacity: 1,
-    scale: 1,
+const videoConAnime = () => {
+  const videoContainer = document.querySelector(".video-container");
+  const play = document.querySelector(".play");
+
+  videoContainer.addEventListener("mouseenter", () => {
+    gsap.to(play, {
+      opacity: 1,
+      scale: 1,
+    });
   });
-});
-videoContainer.addEventListener("mouseleave", () => {
-  gsap.to(play, {
-    opacity: 0,
-    scale: 0,
+  videoContainer.addEventListener("mouseleave", () => {
+    gsap.to(play, {
+      opacity: 0,
+      scale: 0,
+    });
   });
-});
-videoContainer.addEventListener("mousemove", (e) => {
-  gsap.to(play, {
-    left: e.x - 50,
-    top: e.y - 50,
+  videoContainer.addEventListener("mousemove", (e) => {
+    gsap.to(play, {
+      left: e.x - 50,
+      top: e.y - 50,
+    });
   });
-});
-}
+};
 videoConAnime();
 
-gsap.from('#page1 h1',{
-    y: 100,
-    opacity: 0,
-    delay: 0.5,
-    duration: 0.8,
+const textAnime = () => {
+  gsap.from(".bonding h1", {
+    top: "100%",
     stagger: 0.3,
-})
+  });
+};
+textAnime();
+
+const videoAnime = () => {
+  gsap.from(".video", {
+    top: "10%",
+    opacity: 0,
+    delay: 0.3,
+    duraiton: 0.3,
+  });
+};
+videoAnime();
