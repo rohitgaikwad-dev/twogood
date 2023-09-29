31523fb18150c16789607b094b8b7497799202ed
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-09-29 21:24:44 +0530

    fix mousefollower

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
+let scrollY = 0;
+
 const loco = () => {
   const scroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
   });
+
+  scroll.on("scroll", (args) => {
+    scrollY = args.delta.y;
+  });
 };
 loco();
 
-function videoconAnimation() {
-  var videocon = document.querySelector(".video-container");
-  var playbtn = document.querySelector(".play");
+const videoconAnimation = () => {
+  let videocon = document.querySelector("#video-container");
+  let playbtn = document.querySelector("#play");
+
   videocon.addEventListener("mouseenter", () => {
     gsap.to(playbtn, {
       scale: 1,
-      opacity: 1,
     });
   });
+
   videocon.addEventListener("mouseleave", () => {
     gsap.to(playbtn, {
       scale: 0,
     });
   });
+
   document.addEventListener("mousemove", (e) => {
+    let relX = e.pageX;
+    let relY = e.pageY + scrollY;
+
     gsap.to(playbtn, {
-      left: e.x - 70,
-      top: e.y - 80,
+      x: relX - 75,
+      y: relY - 50,
     });
   });
-}
+};
 videoconAnimation();
 
 const textAnime = () => {
@@ -38,10 +49,30 @@ const textAnime = () => {
 textAnime();
 
 const videoAnime = () => {
-  gsap.from(".video", {
+  gsap.from("video", {
     top: "10%",
     opacity: 0,
     delay: 0.3,
   });
 };
 videoAnime();
+
+const cursorFollow = () => {
+  let cursor = document.querySelector("#cursor");
+  let page3 = document.querySelector("#page3");
+
+  page3.addEventListener("mouseleave", () => {
+    gsap.to(cursor,{
+      scale: 0,
+    })
+  })
+
+  page3.addEventListener("mousemove", (e) => {
+    gsap.to(cursor, {
+      left: e.pageX - 95,
+      top: e.pageY - 95,
+      scale: 1,
+    });
+  });
+};
+cursorFollow();
